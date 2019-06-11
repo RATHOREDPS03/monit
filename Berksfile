dafe3c2e353113be6b5eb07e751b2eab79b0e458
Devendra <devendra.p@rapyder.com>
2019-06-11 13:34:16 +0530

    udpate seven

---
+++ Berksfile
@@ -0,0 +1,3 @@
+source 'https://supermarket.chef.io'
+cookbook 'monit-ng', '~> 2.4.0'
+cookbook 'seven_zip', '= 2.0.2'
--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license 'All Rights Reserved'
 description 'Installs/Configures monit-app'
 long_description 'Installs/Configures monit-app'
 version '0.1.0'
-chef_version '>= 14.0'
+chef_version '>= 12.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
@@ -18,3 +18,7 @@ chef_version '>= 14.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/monit-app'
+#
+
+depends 'monit-ng', '~> 2.4.0'
+depends 'seven_zip', '= 2.0.2'
